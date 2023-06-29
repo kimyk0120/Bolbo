42169e45028695c8346f590fe87788f96485d6bd
kimyk0120 <kimyk0120@hotmail.com>
2023-06-29 17:06:27 +0900

    안드로이드 DB 테스트

--- App.jsx
+++ App.jsx
@@ -39,6 +39,7 @@ const App = () => {
   const [dbi, setdb] = useState();
 
   useEffect(() => {
+    // open database and execute sql
     db = SQLite.openDatabase(
       {
         name: 'test.db', // db 파일은 local.db, 파라미터명은 table 명으로 해야함
@@ -46,14 +47,14 @@ const App = () => {
       },
       DB => {
         console.log('불러오기 성공');
+        console.log(DB);
         setdb(db);
         DB.transaction(tx => {
           tx.executeSql('SELECT * FROM test;', [], (tx, results) => {
-            // console.log(tx);
-            // console.log(results);
+            console.log('execute  success results: ' + JSON.stringify(results));
+            console.log('execute success transaction: ' + JSON.stringify(tx));
             const rows = results.rows;
             for (let i = 0; i < rows.length; i++) {
-              // console.log(rows.item(i));
               const test_id = rows.item(i).test_id;
               const test_user_nm = rows.item(i).test_user_nm;
               console.log(test_id, test_user_nm);
--- android/app/src/main/java/com/bolbo/MainApplication.java
+++ android/app/src/main/java/com/bolbo/MainApplication.java
@@ -9,8 +9,11 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
+// import org.pgsqlite.SQLitePluginPackage;
 
 public class MainApplication extends Application implements ReactApplication {
+
+
 
   private final ReactNativeHost mReactNativeHost =
       new DefaultReactNativeHost(this) {
@@ -25,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+//           packages.add(new SQLitePluginPackage());
           return packages;
         }
 
