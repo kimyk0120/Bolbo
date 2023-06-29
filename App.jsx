8938945e7bcedb55aedf22b39abc7193f5541dea
kimyk0120 <kimyk0120@hotmail.com>
2023-06-30 08:23:19 +0900

    * android test

--- App.jsx
+++ App.jsx
@@ -50,16 +50,45 @@ const App = () => {
         console.log(DB);
         setdb(db);
         DB.transaction(tx => {
-          tx.executeSql('SELECT * FROM test;', [], (tx, results) => {
-            console.log('execute  success results: ' + JSON.stringify(results));
-            console.log('execute success transaction: ' + JSON.stringify(tx));
-            const rows = results.rows;
-            for (let i = 0; i < rows.length; i++) {
-              const test_id = rows.item(i).test_id;
-              const test_user_nm = rows.item(i).test_user_nm;
-              console.log(test_id, test_user_nm);
-            }
-          });
+          console.log('DB.transaction', tx); // 여기까지 성공되고 아래 executeSql 실행 되지 않음
+          // IOS 되는 것이라 안드로이드에서는 뭔가 문제가 있는 것 같음
+          // 1. 테이블을 새로 생성하여 해보기
+          // 2. 조회 시점을 달리하여 해보기
+
+          // select all from test table
+          tx.executeSql(
+            'SELECT * FROM test;',
+            [],
+            (tx, results) => {
+              console.log(
+                'execute success results: ' + JSON.stringify(results),
+              );
+              console.log('execute success transaction: ' + JSON.stringify(tx));
+            },
+            error => {
+              console.log('execute error: ' + error.message);
+            },
+          );
+          // tx.executeSql(
+          //   'SELECT * FROM test;',
+          //   [],
+          //   (tx, results) => {
+          //     console.log(
+          //       'execute success results: ' + JSON.stringify(results),
+          //     );
+          //     console.log('execute success transaction: ' + JSON.stringify(tx));
+          //     const rows = results.rows;
+          //
+          //     for (let i = 0; i < rows.length; i++) {
+          //       const test_id = rows.item(i).test_id;
+          //       const test_user_nm = rows.item(i).test_user_nm;
+          //       console.log(test_id, test_user_nm);
+          //     }
+          //   },
+          //   error => {
+          //     console.log('execute error: ' + error.message);
+          //   },
+          // );
         });
       },
       error => {
