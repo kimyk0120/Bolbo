bbe563373a679771a51375edca164930a3e47e8d
kimyk0120 <kimyk0120@hotmail.com>
2023-06-30 11:24:34 +0900

    * android - SQLite 연동은 되었으나. 파일 안에 데이터들이 조회되지 않음.. row가 0으로 나오고 있음

--- App.jsx
+++ App.jsx
@@ -11,6 +11,7 @@
 import React, {useState, useEffect} from 'react';
 
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -30,8 +31,47 @@ import {
 
 import SQLite from 'react-native-sqlite-storage';
 
-const Test = ({id, nm}) => {
-  return <Text>Test</Text>;
+const Test = ({db}) => {
+  return (
+    <Text>
+      <Button
+        title={'test'}
+        onPress={() => {
+          console.log(db);
+          db.transaction(
+            tx => {
+              console.log('transaction', tx);
+              tx.executeSql(
+                'SELECT * FROM test;',
+                [],
+                (tx, results) => {
+                  console.log(
+                    'execute success results: ' + JSON.stringify(results), // 실행은 되긴 하나 로우가 3개 있어야 되는데 없음 ..
+                  );
+                  console.log(
+                    'execute success transaction: ' + JSON.stringify(tx),
+                  );
+                  // const rows = results.rows;
+                  //
+                  // for (let i = 0; i < rows.length; i++) {
+                  //   const test_id = rows.item(i).test_id;
+                  //   const test_user_nm = rows.item(i).test_user_nm;
+                  //   console.log(test_id, test_user_nm);
+                  // }
+                },
+                error => {
+                  console.log('execute error: ' + error.message);
+                },
+              );
+            },
+            error => {
+              console.log('transaction error: ' + error.message);
+            },
+          );
+        }}
+      />
+    </Text>
+  );
 };
 
 let db;
@@ -42,54 +82,13 @@ const App = () => {
     // open database and execute sql
     db = SQLite.openDatabase(
       {
-        name: 'test.db', // db 파일은 local.db, 파라미터명은 table 명으로 해야함
+        name: 'test.db', // db 파일은 test.db, 파라미터명은 table 명으로 해야함
         location: 'default', // www
       },
       DB => {
         console.log('불러오기 성공');
         console.log(DB);
-        setdb(db);
-        DB.transaction(tx => {
-          console.log('DB.transaction', tx); // 여기까지 성공되고 아래 executeSql 실행 되지 않음
-          // IOS 되는 것이라 안드로이드에서는 뭔가 문제가 있는 것 같음
-          // 1. 테이블을 새로 생성하여 해보기
-          // 2. 조회 시점을 달리하여 해보기
-
-          // select all from test table
-          tx.executeSql(
-            'SELECT * FROM test;',
-            [],
-            (tx, results) => {
-              console.log(
-                'execute success results: ' + JSON.stringify(results),
-              );
-              console.log('execute success transaction: ' + JSON.stringify(tx));
-            },
-            error => {
-              console.log('execute error: ' + error.message);
-            },
-          );
-          // tx.executeSql(
-          //   'SELECT * FROM test;',
-          //   [],
-          //   (tx, results) => {
-          //     console.log(
-          //       'execute success results: ' + JSON.stringify(results),
-          //     );
-          //     console.log('execute success transaction: ' + JSON.stringify(tx));
-          //     const rows = results.rows;
-          //
-          //     for (let i = 0; i < rows.length; i++) {
-          //       const test_id = rows.item(i).test_id;
-          //       const test_user_nm = rows.item(i).test_user_nm;
-          //       console.log(test_id, test_user_nm);
-          //     }
-          //   },
-          //   error => {
-          //     console.log('execute error: ' + error.message);
-          //   },
-          // );
-        });
+        setdb(DB);
       },
       error => {
         console.log('에러발생: ', error);
@@ -102,7 +101,7 @@ const App = () => {
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <Header />
         <View>
-          <Test />
+          <Test db={dbi} />
         </View>
       </ScrollView>
     </SafeAreaView>
