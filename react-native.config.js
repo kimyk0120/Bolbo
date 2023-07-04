f7335d05b0bc716431087549617f03dc0a7bb14a
kimyk0120 <kimyk0120@hotmail.com>
2023-07-04 15:36:13 +0900

    * 오류 해결

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,6 @@
+// 안드로이드 앱 빌드시 에러발생:  [Error: Could not open database]
+// 이후 아래 설정 주석 처리시 SQLite 데이터베이스 접근 가능
+
 // module.exports = {
 //   dependencies: {
 //     'react-native-sqlite-storage': {
