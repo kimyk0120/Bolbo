dccb114e7264c1548aea7aab5f6d9a18ff85e988
kimyk0120 <kimyk0120@hotmail.com>
2023-07-09 16:04:34 +0900

    * re add index

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {SafeAreaView, ScrollView, View} from 'react-native';
+import {SafeAreaView, ScrollView, View, Text} from 'react-native';
 
 import {Header} from 'react-native/Libraries/NewAppScreen';
 
@@ -9,7 +9,9 @@ const App = () => {
     <SafeAreaView>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <Header />
-        <View>{/*<DbTest2 db={dbi} />*/}</View>
+        <View>
+          <Text>test</Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
